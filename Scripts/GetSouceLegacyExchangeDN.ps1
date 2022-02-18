d5a8aaf8fb37a4ef0987e1e5556d40bbf6d773e7
allen20210617 <86033833+allen20210617@users.noreply.github.com>
2022-02-18 11:16:20 +0800

    Add files via upload
    update result count

--- Scripts/GetSouceLegacyExchangeDN.ps1
+++ Scripts/GetSouceLegacyExchangeDN.ps1
@@ -1,10 +1,10 @@
-﻿$invocation = (Get-Variable MyInvocation).Value
+$invocation = (Get-Variable MyInvocation).Value
 $currentPath = (Get-Item $invocation.MyCommand.Path).Directory.FullName
 $timestamp = Get-Date -UFormat %Y%m%d%H%M%S
 $reportPath="$currentPath\SourceLegacyExchangeDN_$timestamp.csv";
 try
 {
-   Get-mailbox|select-object primarysmtpaddress,legacyexchangedn|Export-csv $reportPath -NoTypeInformation -ErrorAction Stop
+   Get-mailbox -resultsize unlimited|select-object primarysmtpaddress,legacyexchangedn|Export-csv $reportPath -NoTypeInformation -ErrorAction Stop
    Write-Host "Finished.  File Path: $($reportPath)"
 }
 catch [System.Exception] 
--- Scripts/GetSouceLegacyExchangeDN_Group.ps1
+++ Scripts/GetSouceLegacyExchangeDN_Group.ps1
@@ -1,10 +1,10 @@
-﻿$invocation = (Get-Variable MyInvocation).Value
+$invocation = (Get-Variable MyInvocation).Value
 $currentPath = (Get-Item $invocation.MyCommand.Path).Directory.FullName
 $timestamp = Get-Date -UFormat %Y%m%d%H%M%S
 $reportPath="$currentPath\SourceLegacyExchangeDN_Group_$timestamp.csv";
 try
 {
-   Get-UnifiedGroup|select-object primarysmtpaddress,legacyexchangedn|Export-csv $reportPath -NoTypeInformation -ErrorAction Stop
+   Get-UnifiedGroup -resultsize unlimited|select-object primarysmtpaddress,legacyexchangedn|Export-csv $reportPath -NoTypeInformation -ErrorAction Stop
    Write-Host "Finished.  File Path: $($reportPath)"
 }
 catch [System.Exception] 
