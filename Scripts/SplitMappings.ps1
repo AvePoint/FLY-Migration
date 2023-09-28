b9e570da4d77c85017be926f648d8791085660e1
allen20210617 <86033833+allen20210617@users.noreply.github.com>
2023-09-28 15:52:44 +0800

    Create SplitMappings.ps1
    add new scripts

---
+++ Scripts/SplitMappings.ps1
@@ -0,0 +1,31 @@
+﻿Install-Module PnP.PowerShell
+$SrcSiteUrl = ""
+$DestSiteUrl = ""
+$OutputFilePath = "C:\xxxxxx\Mapping.csv"
+Connect-PnPOnline -Url $SrcSiteUrl -UseWebLogin
+$allWebs = Get-PnPSubWeb -Recurse
+
+if(Test-Path $OutputFilePath)
+{
+    remove-Item -Recurse -Force $OutputFilePath
+}
+#RootSite
+$Data = New-Object PSObject
+$Data | Add-Member NoteProperty "Source URL"($SrcSiteUrl) 
+$Data | Add-Member NoteProperty "Source object level"("Site collection")
+$Data | Add-Member NoteProperty "Destination URL"($DestSiteUrl)
+$Data | Add-Member NoteProperty "Destination object level"("Site collection")
+$Data | Add-Member NoteProperty "Method"("Merge")
+$Data | Export-CSV $OutputFilePath -NoTypeInformation -Append -Encoding UTF8 -Force
+
+#subsites
+foreach($web in $allWebs)
+{
+    $Data = New-Object PSObject
+    $Data | Add-Member NoteProperty "Source URL"($web.Url) 
+    $Data | Add-Member NoteProperty "Source object level"("Site")
+    $Data | Add-Member NoteProperty "Destination URL"($web.Url.Replace($SrcSiteUrl,$DestSiteUrl))
+    $Data | Add-Member NoteProperty "Destination object level"("Site")
+    $Data | Add-Member NoteProperty "Method"("Merge")
+    $Data | Export-CSV $OutputFilePath -NoTypeInformation -Append -Encoding UTF8 -Force
+}
